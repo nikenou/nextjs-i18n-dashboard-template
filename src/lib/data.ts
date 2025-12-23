4cfce876a21a54b51e21be580457263ec2825898
nikenou <87515863@qq.com>
2025-12-23 13:39:54 +0800

    feature: change some code

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -7,7 +7,7 @@ export async function getUser(): Promise<User> {
       lastName: "Doe",
       email: "john.doe@example.com",
       profileImage:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        "",
     };
 
     setTimeout(() => resolve(user), 500);
@@ -30,49 +30,14 @@ export async function getTeamMembers(): Promise<TeamMember[]> {
         lastName: "Vincent",
         username: "@driesvincent",
         profileImage:
-          "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+          "",
       },
       {
         firstName: "Lindsay",
         lastName: "Walton",
         username: "@lindsaywalton",
         profileImage:
-          "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-      {
-        firstName: "Courtney",
-        lastName: "Henry",
-        username: "@courtneyhenry",
-        profileImage:
-          "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-      {
-        firstName: "Whitney",
-        lastName: "Francis",
-        username: "@whitneyfrancis",
-        profileImage:
-          "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-      {
-        firstName: "Leonard",
-        lastName: "Krasner",
-        username: "@leonardkrasner",
-        profileImage:
-          "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-      {
-        firstName: "Floyd",
-        lastName: "Miles",
-        username: "@floydmiles",
-        profileImage:
-          "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-      {
-        firstName: "Emily",
-        lastName: "Selman",
-        username: "@emilyselman",
-        profileImage:
-          "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+          "",
       },
     ];
 
